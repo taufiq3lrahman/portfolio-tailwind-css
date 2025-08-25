7fe9a9c3d229ee64f27685056e237321e47c97cd
taufiq3lrahman <taufiq.gurup3p3@gmail.com>
2025-08-25 11:04:59 +0700

    penambahan tombon to TOP

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,17 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const fixNav = header.offsetTop;
+    const toTop = document.querySelector('#totop');
 
     if (window.pageYOffset > fixNav){
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+       
     }else{
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
     }
 }
 
